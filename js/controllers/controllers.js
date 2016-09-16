9d9e0a0470f674e3e3f73bad93b0c453b541f624
Felipe Bohnert Paetzold <felipe.paetzold@gmail.com>
2016-09-16 11:42:29 -0300

    #8528 imports

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -88,6 +88,13 @@ app.controller('LoginController', function ($scope, $mdSidenav, $location, $http
                 function (response) {
                     $rootScope.userAuthenticated = response.data;
                     $location.path('/questions');
+                    $http.get($rootScope.serviceBase + "users/ranking/punctuation").then(function (response) {
+                        for (var i = 0; i < response.data.length; i++) {
+                            if (response.data[i].id == $rootScope.userAuthenticated.id) {
+                                $rootScope.rank = i + 1;
+                            }
+                        }
+                    });
                 },
                 function (error) {
                     $rootScope.showToast("E-mail ou senha incorreto.");
