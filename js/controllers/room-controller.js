a964710edb85d64acd4dec8a41d20ef8526d8c97
Felipe Bohnert Paetzold <felipe.paetzold@gmail.com>
2016-11-06 14:16:22 -0200

    #8981 macas

--- js/controllers/room-controller.js
+++ js/controllers/room-controller.js
@@ -1,26 +1,24 @@
 app.controller('RoomController', function ($scope, $http, $rootScope, $location, $routeParams, $timeout, $q) {
 
-    $http.get($rootScope.serviceBase + "users/ranking/punctuation").then(function (response) {
-        for (var i = 0; i < response.data.length; i++) {
-            if (response.data[i].id == $rootScope.userAuthenticated.id) {
-                $rootScope.rank = i + 1;
-            }
-        }
-    });
-
     $http.get($rootScope.serviceBase + "users/get-auth").then(function (response) {
         $rootScope.userAuthenticated = response.data;
+        $http.get($rootScope.serviceBase + "classroom/teacher/" + $rootScope.userAuthenticated.id).then(function (response) {
+            $scope.myRooms = response.data;
+        });
+        $http.get($rootScope.serviceBase + "users/ranking/punctuation").then(function (response) {
+            for (var i = 0; i < response.data.length; i++) {
+                if (response.data[i].id == $rootScope.userAuthenticated.id) {
+                    $rootScope.rank = i + 1;
+                }
+            }
+        });
+        $http.get($rootScope.serviceBase + "classroom").then(function (response) {
+            $scope.rooms = response.data;
+        });
     });
 
     $scope.pageTitle = "Salas de aula";
 
-    $http.get($rootScope.serviceBase + "classroom").then(function (response) {
-        $scope.rooms = response.data;
-    });
-
-    $http.get($rootScope.serviceBase + "classroom").then(function (response) {
-        $scope.myRooms = response.data;
-    });
 
     $scope.createRoom = function (room) {
         room.teacher = $rootScope.userAuthenticated;
@@ -29,13 +27,15 @@ app.controller('RoomController', function ($scope, $http, $rootScope, $location,
             console.log(response.data);
             $location.path('/rooms/' + response.data.id);
         });
-    }
+    };
 
     // GetOne - Chama Sala solicitada
     if ($routeParams.id != null) {
         $http.get($rootScope.serviceBase + "classroom/" + $routeParams.id).then(function (response) {
             $scope.room = response.data;
             $scope.users = $scope.room.students;
+            getNumberApples($scope.room.teacher);
+            console.log($scope.room);
             if ($scope.room == "") {
                 $location.path('/404');
             }
@@ -61,9 +61,23 @@ app.controller('RoomController', function ($scope, $http, $rootScope, $location,
         }, function(error) {
             $rootScope.showToast("Não foi possível encontrar o usuário :(");
         });
-    }
-
+    };
+    var getNumberApples = function (teacher) {
+        $http.get($rootScope.serviceBase + "apples/teacher/" + teacher.id).then(function (response) {
+            $scope.room.teacher.numberApples = response.data.length;
+        })
+    };
 // Maçã
     $scope.addApple = function(teacher) {
-    }
+        var apple = {};
+        apple.teacher = teacher;
+        apple.student = $rootScope.userAuthenticated;
+        $http.post($rootScope.serviceBase + "apples", apple).then(function (response) {
+        getNumberApples(teacher);
+        })
+    };
+
+
+
+
 });
